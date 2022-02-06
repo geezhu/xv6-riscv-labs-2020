fc1f8faec68699f2f0f88c38f8844ece3bbfd0f7
geek-zhu <tel18048714276@gmail.com>
2022-02-06 20:54:34 +0800

    [not executable]add freewalk() signatures for proc_freekpagetable()

--- kernel/vm.c
+++ kernel/vm.c
@@ -16,6 +16,8 @@ pagetable_t kernel_pagetable;
 extern char etext[];  // kernel.ld sets this to end of kernel code.
 
 extern char trampoline[]; // trampoline.S
+
+void        freewalk(pagetable_t pagetable);
 /*
  * create a direct-map page table of process for the kernel.
  */
