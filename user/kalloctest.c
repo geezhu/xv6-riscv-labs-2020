f8e5ba39f866eaccd3cc3106dc4c940a5fedbfeb
geek-zhu <tel18048714276@gmail.com>
2022-02-14 18:40:32 +0800

    use lazy-compatible countfree() from usertests

--- user/kalloctest.c
+++ user/kalloctest.c
@@ -80,20 +80,61 @@ void test1(void)
 int
 countfree()
 {
-  uint64 sz0 = (uint64)sbrk(0);
-  int n = 0;
+    int fds[2];
 
-  while(1){
-    uint64 a = (uint64) sbrk(4096);
-    if(a == 0xffffffffffffffff){
-      break;
+    if(pipe(fds) < 0){
+        printf("pipe() failed in countfree()\n");
+        exit(1);
     }
-    // modify the memory to make sure it's really allocated.
-    *(char *)(a + 4096 - 1) = 1;
-    n += 1;
-  }
-  sbrk(-((uint64)sbrk(0) - sz0));
-  return n;
+
+    int pid = fork();
+
+    if(pid < 0){
+        printf("fork failed in countfree()\n");
+        exit(1);
+    }
+
+    if(pid == 0){
+        close(fds[0]);
+
+        while(1){
+            uint64 a = (uint64) sbrk(4096);
+            if(a == 0xffffffffffffffff){
+                break;
+            }
+
+            // modify the memory to make sure it's really allocated.
+            *(char *)(a + 4096 - 1) = 1;
+
+            // report back one more page.
+            if(write(fds[1], "x", 1) != 1){
+                printf("write() failed in countfree()\n");
+                exit(1);
+            }
+        }
+
+        exit(0);
+    }
+
+    close(fds[1]);
+
+    int n = 0;
+    while(1){
+        char c;
+        int cc = read(fds[0], &c, 1);
+        if(cc < 0){
+            printf("read() failed in countfree()\n");
+            exit(1);
+        }
+        if(cc == 0)
+            break;
+        n += 1;
+    }
+
+    close(fds[0]);
+    wait((int*)0);
+
+    return n;
 }
 
 void test2() {
